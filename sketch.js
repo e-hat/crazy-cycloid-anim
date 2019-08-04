fbb8897b8f0369cdbcf041efc138c6c6130f3657
deadbird11 <blankman@hatfieldfamily.com>
2019-08-04 15:42:43 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,13 +1,20 @@
 let tSlider;
+
 function setup() {
   createCanvas(windowWidth, windowHeight)
   background(255, 255, 255)
   textSize(40)
   textAlign(LEFT, TOP)
   fill(0, 0, 0)
-  tSlider = createSlider(1, 15, 4, 0.5);
-  tSlider.position(width / 2 - 200, 5);
+  tSlider = createSlider(1, 15, 6, 0.5);
   
+  if (typeof window.orientation !== "undefined" ||
+    navigator.userAgent.indexOf('IEMobile' !== -1)) {
+    
+    tSlider.position(30, 5);
+  } else {
+    tSlider.position(30, 5);
+  }
 }
 
 let T = 300
@@ -29,24 +36,24 @@ function draw() {
   scale(s)
   clear()
   background(0)
-  
+
   let lastX1 = width / 2 + R,
-      lastY1 = height / 2
-  let lastX2 =  width / 2 + R,
-      lastY2 =  height / 2
-  
+    lastY1 = height / 2
+  let lastX2 = width / 2 + R,
+    lastY2 = height / 2
+
   push()
   translate(width / 2 / s, height / 2 / s)
   strokeWeight(1 / s)
   for (let t = 0; t < T; t += tStep) {
     // x and y of epicycloid
-    let x1 = (r + R) * cos(t) - r* cos((R + r)/r * t),
-        y1 = (r + R) * sin(t) - r * sin((R + r) / r * t)
-    
+    let x1 = (r + R) * cos(t) - r * cos((R + r) / r * t),
+      y1 = (r + R) * sin(t) - r * sin((R + r) / r * t)
+
     // x and y of hypocycloid
-    let x2 = (R - r) * cos(t) + r * cos((R-r)/r * t),
-        y2 = (R - r) * sin(t) - r * sin((R - r) / r * t)
-    
+    let x2 = (R - r) * cos(t) + r * cos((R - r) / r * t),
+      y2 = (R - r) * sin(t) - r * sin((R - r) / r * t)
+
     stroke(255)
     if (t > 0) {
       // drawing lines between points on graph
@@ -64,5 +71,5 @@ function draw() {
   fill(255)
   textSize(15 / s)
   text(tSlider.value(), (tSlider.x - 23) / s, (tSlider.y + 5) / s);
-  
+
 }
